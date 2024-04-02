12d1a5ba67ae60dea7b66df66b277b713835fc8d
Italo Maia <italo.maia@gmail.com>
2024-04-02 19:58:47 +0200

    adding test file to test changes shortcuts

---
+++ test_files/hello_world.py
@@ -0,0 +1,10 @@
+class Hello:
+    def __init__(self, who):
+        self.who = who
+
+    def speak(self):
+        print("Hello %s" %s self.who)
+
+
+def hello_world():
+    Hello("World!").speak()
